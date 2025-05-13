657c904cf169229914dde2455ed5fef091de301c
I .IMRAN <148187844+i-imran09@users.noreply.github.com>
2025-05-13 10:35:44 +0530

    Update firebase-config.js 2

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,5 @@
 // firebase-config.js
-const firebaseConfig = {
-  apiKey: "AIzaSyBt2dyVszD49mERDuDe-J5iEY73x_1R8cU",
-  authDomain: "attendence-1-b58d3.firebaseapp.com",
-  projectId: "attendence-1-b58d3",
-  storageBucket: "attendence-1-b58d3.firebasestorage.app",
-  messagingSenderId: "144493359228",
-  appId: "1:144493359228:web:93e20f08b0ebddb529d9f9",
-  measurementId: "G-QHD86Z8BSL"
+
 };
 
 // Initialize Firebase
